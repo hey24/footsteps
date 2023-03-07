9421222e07b61d00e9f915f8854b93e4f0b1c0f3
Camille Laporte <camillelaporte@me.com>
2023-03-07 12:47:31 +0000

    added index def to controller

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,12 @@
 class HikesController < ApplicationController
+  
+  def index
+    @hikes = Hike.all
+    @markers = @hikes.geocoded.map do |hike|
+      {
+        lat: hike.latitude,
+        lng: hike.longitude
+      }
+    end
+
 end
