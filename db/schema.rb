49ba91c33d65cdfa58ea419598a2fd3cfeb1b435
Ben Heyman <benheyman1@gmail.com>
2023-03-07 15:03:59 +0000

    Updated seed with longitude and latitude for hikes start

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_163838) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_145003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_163838) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "start_latitude"
+    t.float "start_longitude"
     t.index ["user_id"], name: "index_hikes_on_user_id"
   end
 
