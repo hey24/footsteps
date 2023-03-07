503f99561eed1677e47a5eafd300c4b37c4f45a5
Camille Laporte <camillelaporte@me.com>
2023-03-07 16:58:05 +0000

    added search function with autocomplete to map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 // Connects to data-controller="map"
 export default class extends Controller {
@@ -6,16 +7,19 @@ export default class extends Controller {
     apiKey: String,
     markers: Array
   }
-
+  
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-
+    
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
 
     this.#addMarkersToMap()
+
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
   }
   
   #addMarkersToMap() {
@@ -26,10 +30,13 @@ export default class extends Controller {
     })
   this.#fitMapToMarkers()
   }
-
+  
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  
   }
+
+  
 }
