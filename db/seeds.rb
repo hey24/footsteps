7e6969c191ffef648cc090acc1c893fd4dc3c66b
Luke Sherman <lwsherman123@gmail.com>
2023-03-07 12:17:57 +0000

    Created Users in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Creating the seed file"
+puts "Wiping previous seeds"
+User.destroy_all
+
+
+puts "Adding 5 users"
+user1 = User.create(first_name:'test', last_name:'test', email:'test@test.com', password: 123456, description:'A VERY generic person with limited personality.', user_photo_url:'')
+user2 = User.create(first_name:'Luke', last_name:'Sherman', email:'lukesherman@test.com', password: 123456 , description:'Hello I am Luke Sherman', user_photo_url:'')
+user3 = User.create(first_name:'Ben', last_name:'Heyman', email:'benheyman@test.com', password: 123456, description:'Hello I am Ben Heyman', user_photo_url:'')
+user4 = User.create(first_name:'Camille', last_name:'Laporte', email:'camillelaporte@test.com', password: 123456, description:'Hello I am Camille Laporte', user_photo_url:'')
+user5 = User.create(first_name:'Linnea', last_name:'Jonsson', email:'linneajonsson@test.com', password: 123456, description:'Hello I am Linnea Jonsson', user_photo_url:'')
+
+puts "Finished Creating Users"
+puts "Adding some Hikes"
+
+hike1 = Hike.create(hike_name:'', hike_description:'', hike_photo_url:'', difficulty: , distance: , duration: , max_people: , hike_date: , hike_confirmed: , hike_completed: , user_id: )
+hike2 = Hike.create(hike_name:'', hike_description:'', hike_photo_url:'', difficulty: , distance: , duration: , max_people: , hike_date: , hike_confirmed: , hike_completed: , user_id: )
+hike3 = Hike.create(hike_name:'', hike_description:'', hike_photo_url:'', difficulty: , distance: , duration: , max_people: , hike_date: , hike_confirmed: , hike_completed: , user_id: )
+hike4 = Hike.create(hike_name:'', hike_description:'', hike_photo_url:'', difficulty: , distance: , duration: , max_people: , hike_date: , hike_confirmed: , hike_completed: , user_id: )
+hike5 = Hike.create(hike_name:'', hike_description:'', hike_photo_url:'', difficulty: , distance: , duration: , max_people: , hike_date: , hike_confirmed: , hike_completed: , user_id: )
+hike6 = Hike.create(hike_name:'', hike_description:'', hike_photo_url:'', difficulty: , distance: , duration: , max_people: , hike_date: , hike_confirmed: , hike_completed: , user_id: )
