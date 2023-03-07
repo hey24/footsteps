000cce5bdd6037d18d77ab34d5bf4c95cbc334ba
Luke Sherman <lwsherman123@gmail.com>
2023-03-07 16:54:42 +0000

    Created Index and Create methods for requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,25 @@
 class RequestsController < ApplicationController
+
+  def index
+    @user_id = current_user.id
+    @requests = Request.where(user_id: @user_id)
+    @hikes = Hike.where(user_id: @user_id)
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @request.hike = @hike
+    @request.user = current_user
+    if @request.save
+      redirect_to request_path
+    else
+      render 'requests/new', status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def request_params
+    
+  end
 end
