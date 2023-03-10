3fdd3a543bcc2b98c6fab72f96d6a2ac5ebff74a
Ben Heyman <benheyman1@gmail.com>
2023-03-10 10:14:42 +0000

    Added upcoming hikes to my hikes page

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,7 +6,8 @@ class RequestsController < ApplicationController
     @requests = Request.where(user_id: @user_id)
     @pending_requests = Request.where(request_pending: true)
     @hikes = current_user.hikes
-    @accepted_requests = Request.where(user_id: @user_id)
+    @accepted_requests = Request.where(request_accepted: true)
+    # raise
   end
 
   def create
