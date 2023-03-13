313dde999a33d9113c5dc9e742b49c65c6fefc34
Luke Sherman <lwsherman123@gmail.com>
2023-03-13 09:30:39 +0000

    Chat updates automatically for both users"

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,9 +1,7 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
-    def subscribed
-      chatroom = Chatroom.find(params[:id])
-      stream_for chatroom
-    end
+    chatroom = Chatroom.find(params[:id])
+    stream_for chatroom
   end
 
   def unsubscribed
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,11 +4,12 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
-    if @message.save
-      redirect_to chatroom_path(@chatroom)
-    else
-      render "chatrooms/show", status: :unprocessable_entity
-    end
+    @message.save
+    ChatroomChannel.broadcast_to(
+      @chatroom,
+      render_to_string(partial: "messages/message", locals: { message: @message })
+    )
+    head :ok
   end
 
   private
