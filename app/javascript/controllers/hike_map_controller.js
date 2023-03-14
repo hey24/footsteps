6ffd1f0ecf7fcd4e2ede64ba3db853d78385888c
Ben Heyman <benheyman1@gmail.com>
2023-03-14 14:06:19 +0000

    Added pins to start and finish

--- app/javascript/controllers/hike_map_controller.js
+++ app/javascript/controllers/hike_map_controller.js
@@ -6,15 +6,18 @@ export default class extends Controller {
   static values = {
     markerCoordinates: Array,
     startCoordinates: Array,
-    apiKey: String
+    apiKey: String,
+    markerInitialCoordinates: Object,
+    markerFinishCoordinates: Object
   }
 
   connect() {
-    console.log(this.markerCoordinatesValue.length === 0)
-    console.log(this.startCoordinatesValue)
+    // console.log(this.markerInitialCoordinatesValue)
+    // console.log(this.markerCoordinatesValue.length === 0)
+    // console.log(this.startCoordinatesValue)
     if (this.markerCoordinatesValue.length === 0 ) {
       this.centerPoint = this.startCoordinatesValue;
-      console.log('yooooo')
+      // console.log('yooooo')
     } else {
       this.centerPoint = this.markerCoordinatesValue[0];
     }
@@ -26,8 +29,9 @@ export default class extends Controller {
       zoom: 10.5
     });
     this.map.on('load', () => {
-      console.log("load")
+      // console.log("load")
       this.addRouteToMap()
+      this.addMarkersToMap()
     });
   }
   addRouteToMap() {  this.map.addSource('route', {
@@ -52,7 +56,39 @@ export default class extends Controller {
       },
       'paint': {
         'line-color': '#F4B62D',
-        'line-width': 8
+        'line-width': 8,
       }
     });}
+
+    addMarkersToMap() {
+
+        const startMarker = document.createElement("div")
+        startMarker.innerHTML = this.markerInitialCoordinatesValue.marker_html
+        console.log(startMarker)
+
+
+        new mapboxgl.Marker(startMarker)
+          .setLngLat([ this.markerInitialCoordinatesValue.lng, this.markerInitialCoordinatesValue.lat ])
+          .addTo(this.map)
+
+        const finishMarker = document.createElement("div")
+        finishMarker.innerHTML = this.markerFinishCoordinatesValue.marker_html
+        console.log(finishMarker)
+
+        new mapboxgl.Marker(finishMarker)
+          .setLngLat([ this.markerFinishCoordinatesValue.lng, this.markerFinishCoordinatesValue.lat ])
+          .addTo(this.map)
+
+      if (this.markerCoordinatesValue.length != 0) {
+        this.fitMapToMarkers()
+      }
+    };
+
+    fitMapToMarkers() {
+      const bounds = new mapboxgl.LngLatBounds()
+      this.markerCoordinatesValue.forEach(marker => bounds.extend([ marker[0], marker[1] ]))
+      // console.log(bounds)
+      this.map.fitBounds(bounds, { padding: 70, maxZoom: 14, duration: 0 })
+
+    }
 }
